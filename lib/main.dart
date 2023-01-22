8f2d737b8d5bee39cd6b07684aadb72a16345a99
sungheeyoon <torushy@gmail.com>
2023-01-22 22:47:23 +0900

    Added NavBar detail

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
           fontFamily: 'Pretendard',
           textTheme: const TextTheme(
             displayLarge: TextStyle(
--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -41,42 +41,86 @@ class _NavBarState extends State<NavBar> {
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: ImageBox(
-                imageUri: "assets/icons/navbar/home.png",
-                width: 32,
-                height: 32),
-            label: 'Home',
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(16), topLeft: Radius.circular(16)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black38.withOpacity(0.08),
+                spreadRadius: 0,
+                blurRadius: 10,
+                offset: const Offset(0, -2)),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(16.0),
+            topRight: Radius.circular(16.0),
           ),
-          BottomNavigationBarItem(
-            icon: ImageBox(
-                imageUri: "assets/icons/navbar/post.png",
-                width: 32,
-                height: 32),
-            label: 'Post',
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 46),
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: _selectedIndex == 0
+                      ? const ImageBox(
+                          imageUri: "assets/icons/navbar/home_active.png",
+                          width: 32,
+                          height: 32)
+                      : const ImageBox(
+                          imageUri: "assets/icons/navbar/home.png",
+                          width: 32,
+                          height: 32),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: _selectedIndex == 1
+                      ? const ImageBox(
+                          imageUri: "assets/icons/navbar/post_active.png",
+                          width: 32,
+                          height: 32)
+                      : const ImageBox(
+                          imageUri: "assets/icons/navbar/post.png",
+                          width: 32,
+                          height: 32),
+                  label: 'Post',
+                ),
+                BottomNavigationBarItem(
+                  icon: _selectedIndex == 2
+                      ? const ImageBox(
+                          imageUri: "assets/icons/navbar/calendar_active.png",
+                          width: 32,
+                          height: 32)
+                      : const ImageBox(
+                          imageUri: "assets/icons/navbar/calendar.png",
+                          width: 32,
+                          height: 32),
+                  label: 'Calendar',
+                ),
+                BottomNavigationBarItem(
+                  icon: _selectedIndex == 3
+                      ? const ImageBox(
+                          imageUri: "assets/icons/navbar/mypage_active.png",
+                          width: 32,
+                          height: 32)
+                      : const ImageBox(
+                          imageUri: "assets/icons/navbar/mypage.png",
+                          width: 32,
+                          height: 32),
+                  label: 'Mypage',
+                ),
+              ],
+              currentIndex: _selectedIndex,
+              selectedItemColor: Colors.amber[800],
+              onTap: _onItemTapped,
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: ImageBox(
-                imageUri: "assets/icons/navbar/calendar.png",
-                width: 32,
-                height: 32),
-            label: 'Calendar',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageBox(
-                imageUri: "assets/icons/navbar/mypage.png",
-                width: 32,
-                height: 32),
-            label: 'My Page',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+        ),
       ),
     );
   }
