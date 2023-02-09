a3362ac913507e31d4d2352b65973c2b5f3e85be
sungheeyoon <torushy@gmail.com>
2023-02-09 23:47:50 +0900

    Ongoing Search

--- lib/screens/plant_search_screen.dart
+++ lib/screens/plant_search_screen.dart
@@ -3,8 +3,50 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:plant_plan/utils/colors.dart';
 import 'package:plant_plan/widgets/custom_appbar.dart';
 
-class PlantSearchScreen extends StatelessWidget {
+class PlantSearchScreen extends StatefulWidget {
   const PlantSearchScreen({super.key});
+
+  @override
+  State<PlantSearchScreen> createState() => _PlantSearchScreenState();
+}
+
+class _PlantSearchScreenState extends State<PlantSearchScreen> {
+  final List<Map<String, dynamic>> _allUsers = [
+    {"id": 1, "name": "Andy", "age": 29},
+    {"id": 2, "name": "Aragon", "age": 40},
+    {"id": 3, "name": "Bob", "age": 5},
+    {"id": 4, "name": "Barbara", "age": 35},
+    {"id": 5, "name": "Candy", "age": 21},
+    {"id": 6, "name": "Colin", "age": 55},
+    {"id": 7, "name": "Audra", "age": 30},
+    {"id": 8, "name": "가나다라", "age": 14},
+    {"id": 9, "name": "하늘", "age": 100},
+    {"id": 10, "name": "한글", "age": 32},
+  ];
+
+  List<Map<String, dynamic>> _foundUsers = [];
+  @override
+  initState() {
+    _foundUsers = _allUsers;
+    super.initState();
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<Map<String, dynamic>> results = [];
+    if (enteredKeyword.isEmpty) {
+      // if the search field is empty or only contains white-space, we'll display all users
+      results = _allUsers;
+    } else {
+      results = _allUsers
+          .where((user) =>
+              user["name"].toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
+      // we use the toLowerCase() method to make it case-insensitive
+    }
+    setState(() {
+      _foundUsers = results;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +75,7 @@ class PlantSearchScreen extends StatelessWidget {
                 SizedBox(
                   width: MediaQuery.of(context).size.width * 0.7,
                   child: TextField(
+                    onChanged: (value) => _runFilter(value),
                     style: Theme.of(context)
                         .textTheme
                         .bodySmall!
@@ -49,7 +92,35 @@ class PlantSearchScreen extends StatelessWidget {
                   ),
                 ),
               ]),
-            )
+            ),
+            Expanded(
+              child: _foundUsers.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _foundUsers.length,
+                      itemBuilder: (context, index) => Card(
+                        key: ValueKey(_foundUsers[index]["id"]),
+                        color: Colors.blue,
+                        elevation: 4,
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        child: ListTile(
+                          leading: Text(
+                            _foundUsers[index]["id"].toString(),
+                            style: const TextStyle(
+                                fontSize: 24, color: Colors.white),
+                          ),
+                          title: Text(_foundUsers[index]['name'],
+                              style: const TextStyle(color: Colors.white)),
+                          subtitle: Text(
+                              '${_foundUsers[index]["age"].toString()} years old',
+                              style: const TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                    )
+                  : const Text(
+                      'No results found',
+                      style: TextStyle(fontSize: 24),
+                    ),
+            ),
           ],
         ),
       ),
