b5e7bd7d05010fccc5ccf447255f6b17cee23c9c
sungheeyoon <torushy@gmail.com>
2023-02-14 23:46:16 +0900

    Ongoing Firebase image url and uploading

---
+++ lib/controllers/plant_image_controller.dart
@@ -0,0 +1,12 @@
+import 'dart:html';
+
+import 'package:get/get.dart';
+
+class PlantsImageController extends GetxController {
+  getAllPlantImages() {
+    List<String> imgName = ['a', 'b', 'c', 'd'];
+    try {} catch (e) {
+      print(e);
+    }
+  }
+}
