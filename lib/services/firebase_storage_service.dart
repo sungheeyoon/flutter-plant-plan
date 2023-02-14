cce6beb51d31e363ffd43d5b06fa418ba0a27184
sungheeyoon <torushy@gmail.com>
2023-02-14 23:46:16 +0900

    Ongoing Firebase image url and uploading

---
+++ lib/services/firebase_storage_service.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+
+Reference get firebaseStorage => FirebaseStorage.instance.ref();
+
+class FirebaseFirestoreService extends GetxService {
+  Future<String?> getImage(String? imgName) async {
+    if (imgName == null) {
+      return null;
+    }
+    try {
+      var urlRef =
+          firebaseStorage.child('images').child('${imgName.toLowerCase()}.png');
+      var imgUrl = await urlRef.getDownloadURL();
+      return imgUrl;
+    } catch (e) {
+      return null;
+    }
+  }
+}
