2fa8ed3c1c18e9c5b981dfe30d09a580dfc479ad
sungheeyoon <torushy@gmail.com>
2023-02-28 23:26:23 +0900

    Ongoing Notification

--- lib/screens/alarm_screen.dart
+++ lib/screens/alarm_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
+import 'package:plant_plan/services/local_notification_service.dart';
 import 'package:plant_plan/utils/colors.dart';
 import 'package:plant_plan/widgets/custom_appbar.dart';
 
@@ -11,18 +12,119 @@ class AlarmScreen extends StatefulWidget {
 }
 
 class _AlarmScreenState extends State<AlarmScreen> {
+  late final LocalNotificationService service;
+  String? title;
   DateTime _dateTime = DateTime.now();
+
+  @override
+  void initState() {
+    service = LocalNotificationService();
+    service.initialize();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(title: '알람설정', home: false),
       body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
         color: gray5Color,
         child: Column(children: <Widget>[
           const SizedBox(
             height: 10,
           ),
-          hourMinute12H()
+          hourMinute12H(),
+          const SizedBox(
+            height: 22,
+          ),
+          Container(
+            padding: const EdgeInsets.all(12),
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            height: 81,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                '알람 제목',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge!
+                    .copyWith(color: primary3Color),
+              ),
+              SizedBox(
+                height: 40,
+                child: TextFormField(
+                  onChanged: (text) {
+                    setState(() {
+                      title = text;
+                    });
+                  },
+                  textAlign: TextAlign.start,
+                  initialValue: title,
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .copyWith(color: grayBlack),
+                  decoration: InputDecoration(
+                      isDense: true,
+                      contentPadding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                      enabledBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(width: 1, color: gray3Color)),
+                      border: const UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: gray3Color, width: 1.0)),
+                      focusedBorder: const UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: gray3Color, width: 1.0)),
+                      hintText: '선택사항',
+                      hintStyle: Theme.of(context)
+                          .textTheme
+                          .titleMedium!
+                          .copyWith(color: gray2Color)),
+                ),
+              ),
+            ]),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: const EdgeInsets.all(12),
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            height: 72,
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '알람 주기',
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelLarge!
+                        .copyWith(color: primary3Color),
+                  ),
+                  Text(
+                    '다음 알람은 000000000',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodySmall!
+                        .copyWith(color: gray2Color),
+                  )
+                ],
+              )
+            ]),
+          ),
+          ElevatedButton(
+              onPressed: () async {
+                await service.showNotificaion(
+                    id: 0, title: 'Notification Title', body: 'some body');
+                print('clicked');
+              },
+              child: const Text('alarm'))
         ]),
       ),
     );
@@ -36,8 +138,9 @@ class _AlarmScreenState extends State<AlarmScreen> {
           fontSize: 32,
           color: primary3Color.withOpacity(0.2)),
       highlightedTextStyle: const TextStyle(
-          fontWeight: FontWeight.w400, fontSize: 32, color: primary3Color),
+          fontWeight: FontWeight.w700, fontSize: 32, color: primary3Color),
       alignment: Alignment.center,
+      spacing: 40,
       onTimeChange: (time) {
         setState(() {
           _dateTime = time;
