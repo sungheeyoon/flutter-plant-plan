04733fe48ea0e5511a3f77922f59fef97b6f293a
sungheeyoon <torushy@gmail.com>
2023-03-28 23:21:40 +0900

    Sucess getting kakao login access_token

--- lib/user/repository/auth_repository.dart
+++ lib/user/repository/auth_repository.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
@@ -12,28 +13,34 @@ class AuthRepository {
   AuthRepository();
 
   Future<void> login() async {
-    try {
-      bool isInstalled = await isKakaoTalkInstalled();
-      print('로그인중로그인중로그인중로그인중로그인중로그인중로그인중 $isInstalled');
-      if (isInstalled) {
-        OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
+    bool isInstalled = await isKakaoTalkInstalled();
+    if (isInstalled) {
+      try {
+        final kakao = await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공{$kakao}');
+      } catch (error) {
+        print('카카오톡으로 로그인 실패 $error');
 
-        final url = Uri.https('kapi.kakao.com', '/v2/user/me');
-        final resp = await dio.get(
-          '$url',
-          options: Options(
-            headers: {
-              'authorization': 'Bearer ${token.accessToken}',
-            },
-          ),
-        );
-        print('카카오톡으로 로그인 성공 ${token.accessToken}');
-        print(resp.toString());
-      } else {
-        return;
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공');
+        } catch (error) {
+          print('카카오계정으로 로그인 실패 $error');
+        }
       }
-    } catch (error) {
-      print('카카오톡으로 로그인 실패 $error');
+    } else {
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+        print('카카오계정으로 로그인 성공');
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
     }
   }
 }
