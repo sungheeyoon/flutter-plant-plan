fb03315ce3972d80896b36e21f8bb4f0626b9e31
sungheeyoon <torushy@gmail.com>
2023-04-07 23:58:42 +0900

    Ongoing firebaseUI pagination test

---
+++ lib/add/view/search_screen_fireui.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:plant_plan/add/model/plant_model.dart';
+
+class SearchScreenFireUi extends StatelessWidget {
+  const SearchScreenFireUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final platnsQuery = FirebaseFirestore.instance
+        .collection('plant_list')
+        .orderBy('name')
+        .withConverter<PlantModel>(
+          fromFirestore: (snapshot, _) => PlantModel.fromJson(snapshot.data()!),
+          toFirestore: (plant, _) => PlantModel.toJson(),
+        );
+
+    return FirestoreListView<PlantModel>(
+      query: platnsQuery,
+      itemBuilder: (context, snapshot) {
+        PlantModel plant = snapshot.data();
+
+        return Text('plant name is ${plant.name}');
+      },
+    );
+  }
+}
