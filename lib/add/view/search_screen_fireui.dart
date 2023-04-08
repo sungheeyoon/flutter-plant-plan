24bd0c9a8cf692e848acaba2ddafe20bf061f4ff
sungheeyoon <torushy@gmail.com>
2023-04-08 23:41:26 +0900

    Deleted firebase_ui_firestore --dependencies error

--- lib/add/view/search_screen_fireui.dart
+++ lib/add/view/search_screen_fireui.dart
@@ -1,27 +1,28 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:plant_plan/add/model/plant_model.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// import 'package:plant_plan/add/model/plant_model.dart';
+// import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 
-class SearchScreenFireUi extends StatelessWidget {
-  const SearchScreenFireUi({super.key});
+// class SearchScreenFireUi extends StatelessWidget {
+//   const SearchScreenFireUi({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final platnsQuery = FirebaseFirestore.instance
-        .collection('plant_list')
-        .orderBy('name')
-        .withConverter<PlantModel>(
-          fromFirestore: (snapshot, _) => PlantModel.fromJson(snapshot.data()!),
-          toFirestore: (plant, _) => PlantModel.toJson(),
-        );
+//   @override
+//   Widget build(BuildContext context) {
+//     final platnsQuery = FirebaseFirestore.instance
+//         .collection('plant_list')
+//         .orderBy('name')
+//         .withConverter<PlantModel>(
+//           fromFirestore: (snapshot, _) => PlantModel.fromJson(snapshot.data()!),
+//           toFirestore: (plant, _) => plant.toJson(),
+//         );
 
-    return FirestoreListView<PlantModel>(
-      query: platnsQuery,
-      itemBuilder: (context, snapshot) {
-        PlantModel plant = snapshot.data();
+//     return FirestoreListView<PlantModel>(
+//       query: platnsQuery,
+//       itemBuilder: (context, snapshot) {
+//         PlantModel plant = snapshot.data();
 
-        return Text('plant name is ${plant.name}');
-      },
-    );
-  }
-}
+//         return Text('plant name is ${plant.name}');
+//       },
+//     );
+//   }
+// }
