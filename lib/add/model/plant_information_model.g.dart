b68871dd5f7a09902cc291c09c5164e56b16864e
sungheeyoon <torushy@gmail.com>
2023-04-21 23:31:49 +0900

    Editing PlantInformationModel non nullable

--- lib/add/model/plant_information_model.g.dart
+++ lib/add/model/plant_information_model.g.dart
@@ -9,19 +9,13 @@ part of 'plant_information_model.dart';
 _$_PlantInformationModel _$$_PlantInformationModelFromJson(
         Map<String, dynamic> json) =>
     _$_PlantInformationModel(
-      alias: json['alias'] as String?,
-      watering: json['watering'] == null
-          ? null
-          : PlantInformationKey.fromJson(
-              json['watering'] as Map<String, dynamic>),
-      repotting: json['repotting'] == null
-          ? null
-          : PlantInformationKey.fromJson(
-              json['repotting'] as Map<String, dynamic>),
-      nutrient: json['nutrient'] == null
-          ? null
-          : PlantInformationKey.fromJson(
-              json['nutrient'] as Map<String, dynamic>),
+      alias: json['alias'] as String? ?? "",
+      watering: PlantInformationKey.fromJson(
+          json['watering'] as Map<String, dynamic>),
+      repotting: PlantInformationKey.fromJson(
+          json['repotting'] as Map<String, dynamic>),
+      nutrient: PlantInformationKey.fromJson(
+          json['nutrient'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$$_PlantInformationModelToJson(
@@ -36,29 +30,25 @@ Map<String, dynamic> _$$_PlantInformationModelToJson(
 _$_PlantInformationKey _$$_PlantInformationKeyFromJson(
         Map<String, dynamic> json) =>
     _$_PlantInformationKey(
-      day: json['day'] == null ? null : DateTime.parse(json['day'] as String),
-      alarm: json['alarm'] == null
-          ? null
-          : Alarm.fromJson(json['alarm'] as Map<String, dynamic>),
+      day: json['day'] as String? ?? "",
+      alarm: Alarm.fromJson(json['alarm'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$$_PlantInformationKeyToJson(
         _$_PlantInformationKey instance) =>
     <String, dynamic>{
-      'day': instance.day?.toIso8601String(),
+      'day': instance.day,
       'alarm': instance.alarm,
     };
 
 _$_Alarm _$$_AlarmFromJson(Map<String, dynamic> json) => _$_Alarm(
-      startDate: json['startDate'] == null
-          ? null
-          : DateTime.parse(json['startDate'] as String),
-      repeat: json['repeat'] as int?,
-      title: json['title'] as String?,
+      startDate: json['startDate'] as String? ?? "",
+      repeat: json['repeat'] as int? ?? 0,
+      title: json['title'] as String? ?? "",
     );
 
 Map<String, dynamic> _$$_AlarmToJson(_$_Alarm instance) => <String, dynamic>{
-      'startDate': instance.startDate?.toIso8601String(),
+      'startDate': instance.startDate,
       'repeat': instance.repeat,
       'title': instance.title,
     };
