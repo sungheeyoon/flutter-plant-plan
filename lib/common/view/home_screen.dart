b910c19ffdd3b0b3bb11ed12b10fcedd94ce5dd8
sungheeyoon <torushy@gmail.com>
2023-05-13 22:47:38 +0900

    Ongoing HomeScreen UI

--- lib/common/view/home_screen.dart
+++ lib/common/view/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:plant_plan/common/layout/default_layout.dart';
 import 'package:plant_plan/common/widget/home_calendar.dart';
+import 'package:plant_plan/utils/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,16 +10,113 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-        child: SingleChildScrollView(
-      child: SafeArea(
+      backgroundColor: pointColor2,
+      child: SingleChildScrollView(
+        child: SafeArea(
           child: Column(
-        children: [
-          SizedBox(
-            height: 230.h,
-            child: const MyCalendar(),
+            children: [
+              SizedBox(
+                height: 230.h,
+                child: const MyCalendar(),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30), // 모서리 모양 변경
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 24,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.circular(30), // ClipRRect와 동일하게 설정
+                    color: Colors.white,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.circular(32.h), // ClipRRect와 동일하게 설정
+                      color: grayColor100,
+                    ),
+                    height: 56.h,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '해야할 일',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelSmall!
+                                  .copyWith(color: grayColor500),
+                            ),
+                            Text(
+                              '15',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineSmall!
+                                  .copyWith(color: grayBlack),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          width: 1.0,
+                          height: 16.h,
+                          color: grayColor300,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '해야할 일',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelSmall!
+                                  .copyWith(color: grayColor500),
+                            ),
+                            Text(
+                              '15',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineSmall!
+                                  .copyWith(color: grayBlack),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          width: 1.0,
+                          height: 16.h,
+                          color: grayColor300,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '해야할 일',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelSmall!
+                                  .copyWith(color: grayColor500),
+                            ),
+                            Text(
+                              '15',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineSmall!
+                                  .copyWith(color: grayBlack),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
-        ],
-      )),
-    ));
+        ),
+      ),
+    );
   }
 }
