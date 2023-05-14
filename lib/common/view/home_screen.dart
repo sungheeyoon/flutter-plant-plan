5414c661b10d1c90c7162f25d6abc072a802712f
sungheeyoon <torushy@gmail.com>
2023-05-14 23:54:10 +0900

    Added Pageview HomeScreen

--- lib/common/view/home_screen.dart
+++ lib/common/view/home_screen.dart
@@ -4,8 +4,22 @@ import 'package:plant_plan/common/layout/default_layout.dart';
 import 'package:plant_plan/common/widget/home_calendar.dart';
 import 'package:plant_plan/utils/colors.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentPageIndex = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,85 +45,190 @@ class HomeScreen extends StatelessWidget {
                         BorderRadius.circular(30), // ClipRRect와 동일하게 설정
                     color: Colors.white,
                   ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius:
-                          BorderRadius.circular(32.h), // ClipRRect와 동일하게 설정
-                      color: grayColor100,
-                    ),
-                    height: 56.h,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(32.h), // ClipRRect와 동일하게 설정
+                          color: grayColor100,
+                        ),
+                        height: 56.h,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            Text(
-                              '해야할 일',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelSmall!
-                                  .copyWith(color: grayColor500),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  '해야할 일',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelSmall!
+                                      .copyWith(color: grayColor500),
+                                ),
+                                Text(
+                                  '15',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headlineSmall!
+                                      .copyWith(color: grayBlack),
+                                ),
+                              ],
                             ),
-                            Text(
-                              '15',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headlineSmall!
-                                  .copyWith(color: grayBlack),
+                            Container(
+                              width: 1.0,
+                              height: 16.h,
+                              color: grayColor300,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  '해야할 일',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelSmall!
+                                      .copyWith(color: grayColor500),
+                                ),
+                                Text(
+                                  '15',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headlineSmall!
+                                      .copyWith(color: grayBlack),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              width: 1.0,
+                              height: 16.h,
+                              color: grayColor300,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  '해야할 일',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelSmall!
+                                      .copyWith(color: grayColor500),
+                                ),
+                                Text(
+                                  '15',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headlineSmall!
+                                      .copyWith(color: grayBlack),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                        Container(
-                          width: 1.0,
-                          height: 16.h,
-                          color: grayColor300,
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextButton(
+                              onPressed: () {
+                                _pageController.animateToPage(0,
+                                    duration: const Duration(milliseconds: 300),
+                                    curve: Curves.easeInOut);
+                                setState(() {
+                                  _currentPageIndex = 0;
+                                });
+                              },
+                              style: TextButton.styleFrom(
+                                foregroundColor: _currentPageIndex == 0
+                                    ? Colors.white
+                                    : Colors.black,
+                                backgroundColor: _currentPageIndex == 0
+                                    ? primaryColor
+                                    : Colors.white,
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.horizontal(
+                                    left: Radius.circular(30),
+                                    right: Radius.circular(30),
+                                  ),
+                                ),
+                              ),
+                              child: const Text('물주기'),
+                            ),
+                          ),
+                          Expanded(
+                            child: TextButton(
+                              onPressed: () {
+                                _pageController.animateToPage(1,
+                                    duration: const Duration(milliseconds: 300),
+                                    curve: Curves.easeInOut);
+                                setState(() {
+                                  _currentPageIndex = 1;
+                                });
+                              },
+                              style: TextButton.styleFrom(
+                                foregroundColor: _currentPageIndex == 1
+                                    ? Colors.white
+                                    : Colors.black,
+                                backgroundColor: _currentPageIndex == 1
+                                    ? primaryColor
+                                    : Colors.white,
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.horizontal(
+                                    left: Radius.circular(30),
+                                    right: Radius.circular(30),
+                                  ),
+                                ),
+                              ),
+                              child: const Text('분갈이'),
+                            ),
+                          ),
+                          Expanded(
+                            child: TextButton(
+                              onPressed: () {
+                                _pageController.animateToPage(2,
+                                    duration: const Duration(milliseconds: 300),
+                                    curve: Curves.easeInOut);
+                                setState(() {
+                                  _currentPageIndex = 2;
+                                });
+                              },
+                              style: TextButton.styleFrom(
+                                foregroundColor: _currentPageIndex == 2
+                                    ? Colors.white
+                                    : Colors.black,
+                                backgroundColor: _currentPageIndex == 2
+                                    ? primaryColor
+                                    : Colors.white,
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.horizontal(
+                                    left: Radius.circular(30),
+                                    right: Radius.circular(30),
+                                  ),
+                                ),
+                              ),
+                              child: const Text('영양제'),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 300.h,
+                        child: PageView(
+                          controller: _pageController,
+                          onPageChanged: (index) {
+                            setState(() {
+                              _currentPageIndex = index;
+                            });
+                          },
                           children: [
-                            Text(
-                              '해야할 일',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelSmall!
-                                  .copyWith(color: grayColor500),
-                            ),
-                            Text(
-                              '15',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headlineSmall!
-                                  .copyWith(color: grayBlack),
-                            ),
+                            Container(color: Colors.blue),
+                            Container(color: Colors.green),
+                            Container(color: Colors.red),
                           ],
                         ),
-                        Container(
-                          width: 1.0,
-                          height: 16.h,
-                          color: grayColor300,
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              '해야할 일',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelSmall!
-                                  .copyWith(color: grayColor500),
-                            ),
-                            Text(
-                              '15',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headlineSmall!
-                                  .copyWith(color: grayBlack),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               )
