881c8c5699f18704dd0d1a797ce71b43d5a3ffc5
sungheeyoon <torushy@gmail.com>
2023-10-10 21:39:49 +0900

    Added LocalNotificationService delete from prefix

--- lib/services/local_notification_service.dart
+++ lib/services/local_notification_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:plant_plan/add/model/alarm_model.dart';
+import 'package:plant_plan/add/model/plant_model.dart';
 import 'package:plant_plan/add/provider/add_plant_provider.dart';
 import 'package:timezone/timezone.dart' as tz;
 
@@ -64,94 +65,155 @@ class LocalNotificationService {
     print('payload $details');
   }
 
-  Future<void> scheduleAlarmNotification(AlarmModel alarm) async {
-    print('alarm: $alarm');
-    String title = '';
-    String body = '';
-    String channelId = alarm.id;
-    String channelName = '';
-    String channelDescription = '';
-    bool shouldSchedule = alarm.isOn;
+  Future<void> scheduleAlarmNotifications(PlantModel plant) async {
+    String title =
+        plant.information.name + (plant.alias != "" ? '(${plant.alias})' : '');
+    for (AlarmModel alarm in plant.alarms) {
+      String body = '';
+      String channelId = alarm.id;
+      String channelName = '';
+      String channelDescription = '';
 
-    switch (alarm.field) {
-      case PlantField.watering:
-        title = '물주기 알림';
-        body = '오늘은 ${alarm.title}의 물주기 날짜입니다.';
-        channelName = 'watering';
-        channelDescription = '해당 식물의 $title';
-        break;
-      case PlantField.repotting:
-        title = '분갈이 알림';
-        body = '오늘은 ${alarm.title}의 분갈이 날짜입니다.';
-        channelName = 'repotting';
-        channelDescription = '해당 식물의 $title';
-        break;
-      case PlantField.nutrient:
-        title = '영양제 알림';
-        body = '오늘은 ${alarm.title}에게 영양제를 주는 날짜입니다.';
-        channelName = 'nutrient';
-        channelDescription = '해당 식물의 $title';
-        break;
-      case PlantField.none:
-        break;
-    }
-
-    AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails(
-      channelId,
-      channelName,
-      channelDescription: channelDescription,
-      importance: Importance.max,
-      priority: Priority.high,
-      ticker: 'ticker',
-    );
-
-    NotificationDetails platformChannelSpecifics =
-        NotificationDetails(android: androidPlatformChannelSpecifics);
-
-    // 현재 시간부터 첫 번째 알림을 예약
-    await _scheduleNotification(
-      alarm.id.hashCode,
-      title,
-      body,
-      tz.TZDateTime.from(alarm.startTime, tz.local),
-      platformChannelSpecifics,
-      shouldSchedule,
-    );
-
-    // 추가적인 반복 알림 예약
-    if (alarm.repeat > 0) {
-      tz.TZDateTime nextNotificationDate = tz.TZDateTime.from(
-          alarm.startTime.add(Duration(days: alarm.repeat)), tz.local);
-
-      // 현재 시간 이후의 첫 번째 알림으로 설정
-      while (nextNotificationDate.isBefore(tz.TZDateTime.now(tz.local))) {
-        nextNotificationDate =
-            nextNotificationDate.add(Duration(days: alarm.repeat));
+      switch (alarm.field) {
+        case PlantField.watering:
+          body = '$title의 물주기 날짜입니다.';
+          channelName = 'watering';
+          channelDescription = '$title의 물주기 알림';
+          break;
+        case PlantField.repotting:
+          body = '$title의 분갈이 날짜입니다.';
+          channelName = 'repotting';
+          channelDescription = '$title의 분갈이 알림';
+          break;
+        case PlantField.nutrient:
+          body = '$title에게 영양제를 주는 날짜입니다.';
+          channelName = 'nutrient';
+          channelDescription = '$title의 영양제 알림';
+          break;
+        case PlantField.none:
+          break;
       }
 
-      // 반복 알림 예약
-      await _scheduleNotification(
-        alarm.id.hashCode + alarm.repeat,
-        title,
-        body,
-        nextNotificationDate,
-        platformChannelSpecifics,
-        shouldSchedule &&
-            !_isDateInOffDates(nextNotificationDate, alarm.offDates),
+      AndroidNotificationDetails androidPlatformChannelSpecifics =
+          AndroidNotificationDetails(
+        channelId,
+        channelName,
+        channelDescription: channelDescription,
+        importance: Importance.max,
+        priority: Priority.high,
+        ticker: 'ticker',
       );
+
+      NotificationDetails platformChannelSpecifics =
+          NotificationDetails(android: androidPlatformChannelSpecifics);
+      if (alarm.isOn) {
+        if (alarm.repeat == 0) {
+          tz.TZDateTime scheduledDate =
+              tz.TZDateTime.from(alarm.startTime, tz.local);
+          if (scheduledDate.isAfter(tz.TZDateTime.now(tz.local))) {
+            await _localNotificationService.zonedSchedule(
+              alarm.id.hashCode,
+              title,
+              body,
+              scheduledDate,
+              platformChannelSpecifics,
+              uiLocalNotificationDateInterpretation:
+                  UILocalNotificationDateInterpretation.absoluteTime,
+            );
+          }
+        } else if (alarm.repeat > 0) {
+          await scheduleRepeatingNotification(
+            alarm.id.hashCode.toString().substring(0, 6),
+            title,
+            body,
+            tz.TZDateTime.from(alarm.startTime, tz.local),
+            alarm,
+          );
+        }
+      }
     }
   }
 
-  Future<void> _scheduleNotification(
-    int id,
-    String title,
-    String body,
-    tz.TZDateTime scheduledDate,
-    NotificationDetails platformChannelSpecifics,
-    bool shouldSchedule,
-  ) async {
-    // 현재보다 미래거나 같을 때만 알람을 예약
+  Future<void> scheduleRepeatingNotification(String prefix, String title,
+      String body, tz.TZDateTime initialDateTime, AlarmModel alarm) async {
+    int alarmsToSchedule = 5;
+
+    int count = 0;
+    for (int i = 1; i <= alarmsToSchedule; i++) {
+      tz.TZDateTime scheduledDate =
+          initialDateTime.add(Duration(days: i * alarm.repeat));
+
+      if (scheduledDate.isAfter(tz.TZDateTime.now(tz.local)) &&
+          !_isDateInOffDates(scheduledDate, alarm.offDates)) {
+        int actualNotificationId = int.parse('$prefix$count');
+
+        AndroidNotificationDetails androidPlatformChannelSpecifics =
+            const AndroidNotificationDetails(
+          'channel_id',
+          'channel_name',
+          channelDescription: 'channel_description',
+          importance: Importance.high,
+          priority: Priority.high,
+          ticker: 'ticker',
+        );
+
+        NotificationDetails platformChannelSpecifics =
+            NotificationDetails(android: androidPlatformChannelSpecifics);
+
+        await _localNotificationService.zonedSchedule(
+          actualNotificationId,
+          title,
+          body,
+          scheduledDate,
+          platformChannelSpecifics,
+          uiLocalNotificationDateInterpretation:
+              UILocalNotificationDateInterpretation.absoluteTime,
+          payload: scheduledDate.toString(),
+        );
+        count++;
+      }
+    }
+  }
+
+  bool _isDateInOffDates(tz.TZDateTime date, List<DateTime> offDates) {
+    DateTime localDate = date.toLocal();
+
+    return offDates.any((offDate) =>
+        offDate.year == localDate.year &&
+        offDate.month == localDate.month &&
+        offDate.day == localDate.day);
+  }
+
+  Future<void> deleteNotificationsWithPrefix(String prefix) async {
+    List<PendingNotificationRequest> pendingNotifications =
+        await retrievePendingNotifications();
+
+    final realPrefix = prefix.hashCode.toString().substring(0, 6);
+    for (var notification in pendingNotifications) {
+      if (notification.id.toString().startsWith(realPrefix)) {
+        await _localNotificationService.cancel(notification.id);
+        print('Notification with ID ${notification.id} deleted.');
+      } else {
+        print('Notification with ID ${notification.id} not deleted.');
+      }
+    }
+  }
+
+  Future<void> rescheduleNotifications(PlantModel plant) async {
+    await deleteNotificationsWithPrefix(plant.docId);
+
+    await scheduleAlarmNotifications(plant);
+  }
+
+  Future<void> scheduleNotification({
+    required int id,
+    required String title,
+    required String body,
+    required tz.TZDateTime scheduledDate,
+    required NotificationDetails platformChannelSpecifics,
+    required bool shouldSchedule,
+  }) async {
+    // 현재 시간 이후에만 알림을 예약
     if (shouldSchedule && scheduledDate.isAfter(tz.TZDateTime.now(tz.local))) {
       await _localNotificationService.zonedSchedule(
         id,
@@ -161,18 +223,9 @@ class LocalNotificationService {
         platformChannelSpecifics,
         uiLocalNotificationDateInterpretation:
             UILocalNotificationDateInterpretation.absoluteTime,
-        matchDateTimeComponents: DateTimeComponents.time,
-        payload: '$scheduledDate',
+        payload: scheduledDate.toString(),
       );
     }
-  }
-
-  bool _isDateInOffDates(tz.TZDateTime date, List<DateTime> offDates) {
-    return offDates.any((offDate) {
-      return date.year == offDate.year &&
-          date.month == offDate.month &&
-          date.day == offDate.day;
-    });
   }
 
   Future<void> deleteAllNotifications() async {
