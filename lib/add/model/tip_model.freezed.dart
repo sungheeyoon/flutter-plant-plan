6c92fd95ac29ed6969267e8e8c0e0fdc3e9f8e48
sungheeyoon <torushy@gmail.com>
2023-10-19 23:47:27 +0900

    Finished AddDirectly

--- lib/add/model/tip_model.freezed.dart
+++ lib/add/model/tip_model.freezed.dart
@@ -21,7 +21,6 @@ TipModel _$TipModelFromJson(Map<String, dynamic> json) {
 /// @nodoc
 mixin _$TipModel {
   String get part => throw _privateConstructorUsedError;
-  String get title => throw _privateConstructorUsedError;
   String get context => throw _privateConstructorUsedError;
 
   Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
@@ -35,7 +34,7 @@ abstract class $TipModelCopyWith<$Res> {
   factory $TipModelCopyWith(TipModel value, $Res Function(TipModel) then) =
       _$TipModelCopyWithImpl<$Res, TipModel>;
   @useResult
-  $Res call({String part, String title, String context});
+  $Res call({String part, String context});
 }
 
 /// @nodoc
@@ -52,17 +51,12 @@ class _$TipModelCopyWithImpl<$Res, $Val extends TipModel>
   @override
   $Res call({
     Object? part = null,
-    Object? title = null,
     Object? context = null,
   }) {
     return _then(_value.copyWith(
       part: null == part
           ? _value.part
           : part // ignore: cast_nullable_to_non_nullable
-              as String,
-      title: null == title
-          ? _value.title
-          : title // ignore: cast_nullable_to_non_nullable
               as String,
       context: null == context
           ? _value.context
@@ -79,7 +73,7 @@ abstract class _$$_TipModelCopyWith<$Res> implements $TipModelCopyWith<$Res> {
       __$$_TipModelCopyWithImpl<$Res>;
   @override
   @useResult
-  $Res call({String part, String title, String context});
+  $Res call({String part, String context});
 }
 
 /// @nodoc
@@ -94,17 +88,12 @@ class __$$_TipModelCopyWithImpl<$Res>
   @override
   $Res call({
     Object? part = null,
-    Object? title = null,
     Object? context = null,
   }) {
     return _then(_$_TipModel(
       part: null == part
           ? _value.part
           : part // ignore: cast_nullable_to_non_nullable
-              as String,
-      title: null == title
-          ? _value.title
-          : title // ignore: cast_nullable_to_non_nullable
               as String,
       context: null == context
           ? _value.context
@@ -117,7 +106,7 @@ class __$$_TipModelCopyWithImpl<$Res>
 /// @nodoc
 @JsonSerializable()
 class _$_TipModel implements _TipModel {
-  _$_TipModel({this.part = '', this.title = '', this.context = ''});
+  _$_TipModel({this.part = '', this.context = ''});
 
   factory _$_TipModel.fromJson(Map<String, dynamic> json) =>
       _$$_TipModelFromJson(json);
@@ -127,14 +116,11 @@ class _$_TipModel implements _TipModel {
   final String part;
   @override
   @JsonKey()
-  final String title;
-  @override
-  @JsonKey()
   final String context;
 
   @override
   String toString() {
-    return 'TipModel(part: $part, title: $title, context: $context)';
+    return 'TipModel(part: $part, context: $context)';
   }
 
   @override
@@ -143,13 +129,12 @@ class _$_TipModel implements _TipModel {
         (other.runtimeType == runtimeType &&
             other is _$_TipModel &&
             (identical(other.part, part) || other.part == part) &&
-            (identical(other.title, title) || other.title == title) &&
             (identical(other.context, context) || other.context == context));
   }
 
   @JsonKey(ignore: true)
   @override
-  int get hashCode => Object.hash(runtimeType, part, title, context);
+  int get hashCode => Object.hash(runtimeType, part, context);
 
   @JsonKey(ignore: true)
   @override
@@ -166,17 +151,12 @@ class _$_TipModel implements _TipModel {
 }
 
 abstract class _TipModel implements TipModel {
-  factory _TipModel(
-      {final String part,
-      final String title,
-      final String context}) = _$_TipModel;
+  factory _TipModel({final String part, final String context}) = _$_TipModel;
 
   factory _TipModel.fromJson(Map<String, dynamic> json) = _$_TipModel.fromJson;
 
   @override
   String get part;
-  @override
-  String get title;
   @override
   String get context;
   @override
--- lib/add/provider/information_provider.dart
+++ lib/add/provider/information_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:plant_plan/add/model/information_model.dart';
+import 'package:plant_plan/add/model/tip_model.dart';
 
 final informationProvider =
     StateNotifierProvider<InformationNotifier, InformationModel>(
@@ -19,7 +20,23 @@ class InformationNotifier extends StateNotifier<InformationModel> {
     state = InformationModel();
   }
 
+  void updateId(String id) {
+    state = state.copyWith(id: id);
+  }
+
   void updateImageUrl(String imageUrl) {
     state = state.copyWith(imageUrl: imageUrl);
+  }
+
+  void updateName(String name) {
+    state = state.copyWith(name: name);
+  }
+
+  void updateTips(List<TipModel> tips) {
+    state = state.copyWith(tips: tips);
+  }
+
+  void reset() {
+    state = InformationModel();
   }
 }
