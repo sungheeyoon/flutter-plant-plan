c417c6c9b07c829990c8b5727f6fbc0b518781ad
sungheeyoon <torushy@gmail.com>
2023-11-09 23:59:17 +0900

    Added ForgotPasswordScreen

--- lib/common/view/login_screen.dart
+++ lib/common/view/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:plant_plan/common/layout/default_layout.dart';
+import 'package:plant_plan/common/view/forgot_password_screen.dart';
 import 'package:plant_plan/common/view/root_tab.dart';
 import 'package:plant_plan/common/view/sign_up_form.dart';
 import 'package:plant_plan/common/widget/input_box.dart';
@@ -26,7 +27,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
     if (_loginKey.currentState!.saveAndValidate()) {
       final formData = _loginKey.currentState!.value;
       setState(() {
-        _errorText = ''; // Reset error message
+        _errorText = '';
       });
 
       if (formData['email'] == null || formData['email'].isEmpty) {
@@ -98,173 +99,217 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
   Widget build(BuildContext context) {
     return DefaultLayout(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 100.h,
-              ),
-              Text(
-                "내 식물을 위한 플랜",
-                style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                      color: primaryColor,
-                    ),
-              ),
-              SizedBox(
-                height: 12.h,
-              ),
-              const Image(
-                image: AssetImage('assets/images/login/logo.png'),
-                width: 122,
-                height: 46,
-                fit: BoxFit.contain,
-              ),
-              SizedBox(
-                height: 80.h,
-              ),
-              FormBuilder(
-                key: _loginKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const InputBox(
-                      name: 'email',
-                      title: '이메일',
-                      hintText: '이메일을 입력해주세요',
-                    ),
-                    SizedBox(height: 12.0.h),
-                    const InputBox(
-                      name: 'password',
-                      title: '비밀번호',
-                      hintText: '비밀번호를 입력해주세요',
-                      isPassword: true,
-                    ),
-                    SizedBox(height: 8.0.h),
-                    Text(
-                      _errorText,
-                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                            color: Colors.red,
-                          ),
-                    ),
-                    SizedBox(height: 36.0.h),
-                    ElevatedButton(
-                      onPressed: () => _login(context),
-                      style: ButtonStyle(
-                        elevation: MaterialStateProperty.all(0),
-                        minimumSize: MaterialStateProperty.all(
-                            const Size(double.infinity, 42)), // 최대 너비 설정
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        backgroundColor: MaterialStateProperty.all(pointColor2),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+        child: CustomScrollView(slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(
+                  height: 88,
+                ),
+                Text(
+                  "내 식물을 위한 플랜",
+                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                        color: primaryColor,
                       ),
-                      child: Text(
-                        '로그인',
-                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                              color: Colors.white,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const Image(
+                  image: AssetImage('assets/images/login/logo.png'),
+                  width: 122,
+                  height: 46,
+                  fit: BoxFit.contain,
+                ),
+                const SizedBox(
+                  height: 90,
+                ),
+                FormBuilder(
+                  key: _loginKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      InputBox(
+                          name: 'email',
+                          title: '이메일',
+                          hintText: '이메일을 입력해주세요',
+                          validator: (val) {
+                            if (val == null) {
+                              return '이메일을 입력해주세요';
+                            }
+                            return null;
+                          }),
+                      SizedBox(height: 12.0.h),
+                      InputBox(
+                          name: 'password',
+                          title: '비밀번호',
+                          hintText: '비밀번호를 입력해주세요',
+                          isPassword: true,
+                          validator: (val) {
+                            if (val == null) {
+                              return '비밀번호를 입력해주세요';
+                            }
+                            return null;
+                          }),
+                      SizedBox(height: 12.0.h),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return const ForgotPasswordScreen();
+                          }));
+                        },
+                        child: Text(
+                          '비밀번호를 찾고싶어요',
+                          textAlign: TextAlign.right,
+                          style:
+                              Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    color: grayColor500,
+                                  ),
+                        ),
+                      ),
+                      SizedBox(height: 36.0.h),
+                      ElevatedButton(
+                        onPressed: () => _login(context),
+                        style: ButtonStyle(
+                          elevation: MaterialStateProperty.all(0),
+                          minimumSize: MaterialStateProperty.all(
+                              const Size(double.infinity, 42)), // 최대 너비 설정
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          backgroundColor:
+                              MaterialStateProperty.all(pointColor2),
+                        ),
+                        child: Text(
+                          '로그인',
+                          style:
+                              Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                    color: Colors.white,
+                                  ),
+                        ),
+                      ),
+                      SizedBox(height: 8.0.h),
+                      Text(
+                        _errorText,
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              color: Colors.red,
                             ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 60.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Expanded(
-                    child: Divider(
-                      thickness: 1,
-                      color: Colors.grey, // 변경된 색상
-                    ),
-                  ),
-                  const SizedBox(width: 8.0),
-                  Text(
-                    '또는 간편로그인하기',
-                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                          color: grayColor700,
+                //간편 로그인 추가예정
+                // SizedBox(
+                //   height: 60.h,
+                // ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: [
+                //     const Expanded(
+                //       child: Divider(
+                //         thickness: 1,
+                //         color: Colors.grey, // 변경된 색상
+                //       ),
+                //     ),
+                //     const SizedBox(width: 8.0),
+                //     Text(
+                //       '또는 간편로그인하기',
+                //       style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                //             color: grayColor700,
+                //           ),
+                //     ),
+                //     const SizedBox(width: 8.0),
+                //     const Expanded(
+                //       child: Divider(
+                //         thickness: 1,
+                //         color: Colors.grey, // 변경된 색상
+                //       ),
+                //     ),
+                //   ],
+                // ),
+                // SizedBox(
+                //   height: 20.h,
+                // ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: [
+                //     Image(
+                //       image: const AssetImage('assets/images/login/kakao.png'),
+                //       width: 56.h,
+                //       height: 56.h,
+                //       fit: BoxFit.contain,
+                //     ),
+                //     const SizedBox(
+                //       width: 16.0,
+                //     ),
+                //     Image(
+                //       image: const AssetImage('assets/images/login/naver.png'),
+                //       width: 56.h,
+                //       height: 56.h,
+                //       fit: BoxFit.contain,
+                //     ),
+                //     const SizedBox(
+                //       width: 16.0,
+                //     ),
+                //     Image(
+                //       image: const AssetImage('assets/images/login/google.png'),
+                //       width: 58.h,
+                //       height: 58.h,
+                //       fit: BoxFit.contain,
+                //     ),
+                //   ],
+                // ),
+                // SizedBox(
+                //   height: 60.h,
+                // ),
+
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '아직 식플의 회원이 아니신가요?',
+                          style:
+                              Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    color: grayColor600,
+                                  ),
                         ),
-                  ),
-                  const SizedBox(width: 8.0),
-                  const Expanded(
-                    child: Divider(
-                      thickness: 1,
-                      color: Colors.grey, // 변경된 색상
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image(
-                    image: const AssetImage('assets/images/login/kakao.png'),
-                    width: 56.h,
-                    height: 56.h,
-                    fit: BoxFit.contain,
-                  ),
-                  const SizedBox(
-                    width: 16.0,
-                  ),
-                  Image(
-                    image: const AssetImage('assets/images/login/naver.png'),
-                    width: 56.h,
-                    height: 56.h,
-                    fit: BoxFit.contain,
-                  ),
-                  const SizedBox(
-                    width: 16.0,
-                  ),
-                  Image(
-                    image: const AssetImage('assets/images/login/google.png'),
-                    width: 58.h,
-                    height: 58.h,
-                    fit: BoxFit.contain,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 60.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '아직 식플의 회원이 아니신가요?',
-                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                          color: grayColor600,
-                        ),
-                  ),
-                  SizedBox(width: 8.h),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return const SignUpForm();
-                      }));
-                    },
-                    child: Text(
-                      '회원가입',
-                      style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                            color: pointColor2,
+                        SizedBox(width: 8.h),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return const SignUpForm();
+                            }));
+                          },
+                          child: Text(
+                            '회원가입',
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelMedium!
+                                .copyWith(
+                                  color: pointColor2,
+                                ),
                           ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
-        ),
+                ),
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
